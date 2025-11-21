9bbccfdb2bc3224a84891656546d51bc2e174101
0001159198-rgb <0001159198@senaimgaluno.com.br>
2025-11-21 17:40:36 -0300

    Update LutadorMedio.java

--- LutadorMedio.java
+++ LutadorMedio.java
@@ -1,28 +1,23 @@
 public class LutadorMedio extends lutador {
 
     public LutadorMedio(String nome) {
-        super(nome, 120, 100, 15, 10); 
-    }
-
-    @Override
-    public void atacar(lutador oponente) {
-        int dano = getForca(); // ataque normal
-        oponente.receberDano(dano);
-        System.out.println(getNome() + " atacou causando " + dano + " de dano!");
+        super(nome, 120, 100, 16, 10, 100);
     }
 
     @Override
     public void especial(lutador oponente) {
-        if (getEnergia() < 20) {
-            System.out.println(getNome() + " não tem energia para o ESPECIAL!");
+        int custo = 25;
+
+        if (energia < custo) {
+            System.out.println(nome + " não tem energia para o especial!");
             return;
         }
 
-        setEnergia(getEnergia() - 20);
+        energia -= custo;
 
-        int dano = getForca() * 3;
+        int dano = forca * 3;
         oponente.receberDano(dano);
 
-        System.out.println(getNome() + " usou o ESPECIAL MÉDIO causando " + dano + " de dano!");
+        System.out.println(nome + " usou ESPECIAL MÉDIO causando " + dano + " de dano!");
     }
 }
