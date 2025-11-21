ea1f0c78b74f3439f18118c5ad3ed3b2c18b6e18
0001159198-rgb <0001159198@senaimgaluno.com.br>
2025-11-21 17:41:43 -0300

    Update lutador.java

--- lutador.java
+++ lutador.java
@@ -1,173 +1,78 @@
-import java.util.Random;
-import java.util.Scanner;
+public abstract class lutador {
+    protected String nome;
+    protected int vida;
+    protected int energia;
+    protected int forca;
+    protected int defesa;
+    protected int energiaMax;
+    protected int defesaTemp = 0;
 
-// =================== CLASSE LUTADOR ===================
-public class lutador {
-    private String nome;
-    private int vida;
-    private int energia;
-    private int forca;
-    private int energiaMax;
-    private int defesa;
-    private int defesaTemporaria = 0;
-
-    public lutador(String nome, int vida, int energia, int forca, int energiaMax, int defesa) {
+    public lutador(String nome, int vida, int energia, int forca, int defesa, int energiaMax) {
         this.nome = nome;
         this.vida = vida;
         this.energia = energia;
         this.forca = forca;
-        this.energiaMax = energiaMax;
         this.defesa = defesa;
+        this.energiaMax = energiaMax;
     }
 
-    // Getters e setters
     public String getNome() { return nome; }
     public int getVida() { return vida; }
     public int getEnergia() { return energia; }
-    public int getEnergiaMax() { return energiaMax; }
     public int getForca() { return forca; }
-    public int getDefesa() { return defesa; }
-    public int getDefesaTemporaria() { return defesaTemporaria; }
 
-    public void setVida(int vida) { this.vida = vida; }
-    public void setEnergia(int energia) { this.energia = energia; }
+    public void setEnergia(int e) {
+        energia = Math.min(e, energiaMax);
+    }
 
-    // ----------------- TOMAR DANO -----------------
-    public void tomarDano(int dano) {
+    public boolean estaVivo() {
+        return vida > 0;
+    }
 
-        if (defesaTemporaria > 0) {
-            dano -= defesaTemporaria;
-            if (dano < 0) dano = 0;
+    // REGRAS DE NEGÓCIO: HP não pode ficar negativo
+    public void receberDano(int dano) {
+        dano -= defesaTemp;
+        defesaTemp = 0;
 
-            System.out.println(nome + " reduziu o dano em " + defesaTemporaria);
-            defesaTemporaria = 0;
-        }
+        if (dano < 0) dano = 0;
 
         vida -= dano;
+
         if (vida < 0) vida = 0;
+
+        System.out.println(nome + " recebeu " + dano + " de dano!");
     }
 
-    // ----------------- ATAQUE -----------------
+    // REGRAS DE COMBATE: só ataca se ambos vivos
     public void atacar(lutador oponente) {
-        oponente.tomarDano(this.forca);
-        System.out.println(this.nome + " atacou " + oponente.getNome() + " causando " + this.forca + " de dano!");
-    }
-
-    // ----------------- ESPECIAL -----------------
-    public void especial(lutador oponente) {
-        int custo = 20;
-        int danoEspecial = this.forca + (this.energia / 2);
-
-        if (this.energia < custo) {
-            System.out.println(this.nome + " não tem energia suficiente para o ESPECIAL!");
+        if (!estaVivo()) {
+            System.out.println(nome + " não pode atacar, está derrotado!");
+            return;
+        }
+        if (!oponente.estaVivo()) {
+            System.out.println("Oponente já está derrotado!");
             return;
         }
 
-        this.energia -= custo;
-        oponente.tomarDano(danoEspecial);
+        int dano = forca;
+        oponente.receberDano(dano);
 
-        System.out.println(this.nome + " usou o ESPECIAL causando " + danoEspecial + " de dano em " + oponente.getNome());
+        System.out.println(nome + " atacou causando " + dano + " de dano!");
     }
 
-    // ----------------- DEFENDER -----------------
+    // ESPECIAL precisa validar energia
+    public abstract void especial(lutador oponente);
+
     public void defender() {
-        Random r = new Random();
-        int sorte = r.nextInt(2); // 0 ou 1
-
-        if (sorte == 0) {
-            int energiaGanha = 5;
-            this.energia += energiaGanha;
-
-            if (this.energia > this.energiaMax)
-                this.energia = this.energiaMax;
-
-            System.out.println(this.nome + " se defendeu e recuperou " + energiaGanha + " de energia!");
-        } else {
-            defesaTemporaria = this.forca / 3;
-            System.out.println(this.nome + " ergueu a guarda e reduzirá " + defesaTemporaria + " de dano no próximo golpe!");
-        }
+        defesaTemp = defesa;
+        energia = Math.min(energia + 10, energiaMax);
+        System.out.println(nome + " está defendendo (+defesa, +energia).");
     }
 
     public void mostrarStatus() {
-        System.out.println("\n===== STATUS DE " + nome + " =====");
+        System.out.println("---- " + nome + " ----");
         System.out.println("Vida: " + vida);
         System.out.println("Energia: " + energia + "/" + energiaMax);
-        System.out.println("==============================\n");
-    }
-
-    public boolean isVivo() { return vida > 0; }
-}
-
-
-
-// =================== CLASSE MAIN ===================
-class batalha {
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-        Random r = new Random();
-
-        // Criar os lutadores
-        lutador jogador = new lutador("Jogador", 100, 50, 20, 50, 10);
-        lutador inimigo = new lutador("Inimigo", 100, 50, 18, 50, 8);
-
-        System.out.println("=== INÍCIO DA BATALHA ===");
-
-        // -------- LOOP DA LUTA --------
-        while (jogador.isVivo() && inimigo.isVivo()) {
-
-            jogador.mostrarStatus();
-            inimigo.mostrarStatus();
-
-            // Turno do jogador
-            System.out.println("Escolha sua ação:");
-            System.out.println("1 - Ataque normal");
-            System.out.println("2 - Golpe especial");
-            System.out.println("3 - Defender");
-            System.out.print("Opção: ");
-
-            int acao = sc.nextInt();
-
-            switch (acao) {
-                case 1:
-                    jogador.atacar(inimigo);
-                    break;
-                case 2:
-                    jogador.especial(inimigo);
-                    break;
-                case 3:
-                    jogador.defender();
-                    break;
-                default:
-                    System.out.println("Opção inválida! Perdeu o turno!");
-            }
-
-            // Verifica se o inimigo morreu
-            if (!inimigo.isVivo()) break;
-
-            // Turno do inimigo (aleatório)
-            int acaoInimigo = r.nextInt(3) + 1;
-
-            if (acaoInimigo == 1) {
-                inimigo.atacar(jogador);
-            } else if (acaoInimigo == 2) {
-                inimigo.especial(jogador);
-            } else {
-                inimigo.defender();
-            }
-
-            // Verifica se o jogador morreu
-            if (!jogador.isVivo()) break;
-
-            System.out.println("\n---------------------------------\n");
-        }
-
-        // -------- FIM DA BATALHA --------
-        if (jogador.isVivo())
-            System.out.println("\n🎉 O jogador venceu a batalha!");
-        else
-            System.out.println("\n💀 O inimigo venceu!");
-
-        sc.close();
+        System.out.println("----------------------");
     }
 }
