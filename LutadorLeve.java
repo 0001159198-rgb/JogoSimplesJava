84a77efa0816f88d896d84ccf5013769aa4a05cd
0001159198-rgb <0001159198@senaimgaluno.com.br>
2025-11-21 17:40:08 -0300

    Update LutadorLeve.java

--- LutadorLeve.java
+++ LutadorLeve.java
@@ -1,28 +1,23 @@
 public class LutadorLeve extends lutador {
 
     public LutadorLeve(String nome) {
-        super(nome, 80, 100, 10, 5); 
-    }
-
-    @Override
-    public void atacar(lutador oponente) {
-        int dano = getForca(); // leve bate fraco
-        oponente.receberDano(dano);
-        System.out.println(getNome() + " atacou causando " + dano + " de dano!");
+        super(nome, 90, 100, 12, 8, 100);
     }
 
     @Override
     public void especial(lutador oponente) {
-        if (getEnergia() < 15) {
-            System.out.println(getNome() + " não tem energia para o ESPECIAL!");
+        int custo = 20;
+
+        if (energia < custo) {
+            System.out.println(nome + " não tem energia para o especial!");
             return;
         }
 
-        setEnergia(getEnergia() - 15);
+        energia -= custo;
 
-        int dano = getForca() * 2; // leve tem especial moderado
+        int dano = forca * 3;
         oponente.receberDano(dano);
 
-        System.out.println(getNome() + " usou o ESPECIAL LEVE causando " + dano + " de dano!");
+        System.out.println(nome + " usou ESPECIAL LEVE causando " + dano + " de dano!");
     }
 }
